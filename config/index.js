da05ab2fb1fe754ba2b22ae217803a8370b497b2
anymouschina <624144061@qq.com>
2019-11-12 18:52:52 +0800

    change port

--- config/index.js
+++ config/index.js
@@ -3,8 +3,8 @@ const { env } = process;
 const config = {
   // host: env.HOST,
   // port: env.PORT,
-  // host:'127.0.0.1',
-  port:3000,
+  host:'47.100.7.95',
+  port:80,
   jwtSecret:process.env.JWT_SECRET,
   wxSecret:'ca008a164dfa01913a6ec8edbcbe2cf7',
   wxAppid:'wx536dfc5d38954079'
