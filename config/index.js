d58611ce30f297b2b51bfec3025990ebb09b26f6
anymouschina <624144061@qq.com>
2019-11-12 19:49:07 +0800

    host changed

--- config/index.js
+++ config/index.js
@@ -3,8 +3,8 @@ const { env } = process;
 const config = {
   // host: env.HOST,
   // port: env.PORT,
-  host:'47.100.7.95',
-  port:3000,
+  host:'0.0.0.0',
+  port:80,
   jwtSecret:process.env.JWT_SECRET,
   wxSecret:'ca008a164dfa01913a6ec8edbcbe2cf7',
   wxAppid:'wx536dfc5d38954079'
