658b0017ec9a2a53708704b2ca3036ac0e2c0e61
anymouschina <624144061@qq.com>
2019-11-23 11:30:30 +0800

    返回数据修改

--- utils/getgoodsInfo.js
+++ utils/getgoodsInfo.js
@@ -3,7 +3,10 @@ const request = require('request')
 module.exports = (barcode,reply)=>request(`https://www.mxnzp.com/api/barcode/goods/details?barcode=${barcode}&appid=wx536dfc5d38954079&secret=ca008a164dfa01913a6ec8edbcbe2cf7`,((err,res)=>{
     if(err)throw err
     else {
-        reply(res.body)
+        reply({
+            status:200,
+            data:res.body.data
+        })
         // console.log(res.body)
     }
 }))
