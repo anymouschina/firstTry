7faffb960cde06891015c577d1c91fd4dcf43bc8
unknown <624144061@qq.com>
2019-12-23 11:09:14 +0800

    删除接口修改

--- routes/goods.js
+++ routes/goods.js
@@ -56,12 +56,8 @@ module.exports = [
     path: `/${GROUP_NAME}/deleteByBarcode`,
     handler: async (request, reply) => {
         let {barcode,open_id} = request.query
-        models.goods.deleteMany({isbn13:barcode,open_id}).then((err,res)=>{
-          if(err){
-            throw err 
-          }else{
-            reply(res)
-          }
+        models.goods.deleteMany({isbn13:barcode,open_id}).then((res)=>{
+            reply({statu:200,data:res})
         })
     },
     config: {
