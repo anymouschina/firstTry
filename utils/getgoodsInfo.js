617615e1b4363292eea5a627264039fb0ad93266
unknown <624144061@qq.com>
2019-12-23 09:56:38 +0800

    add join

--- utils/getgoodsInfo.js
+++ utils/getgoodsInfo.js
@@ -1,6 +1,6 @@
 const request = require('request')
 let total = 200
-const dealBarcode = (barcode,reply,models,open_id)=>request(`https://api.douban.com/v2/book/isbn/:${barcode}?apikey=0df993c66c0c636e29ecbb5344252a4a`,((urlErr,urlRes)=>{
+const dealBarcode = (barcode,reply,models,open_id,join)=>request(`https://api.douban.com/v2/book/isbn/:${barcode}?apikey=0df993c66c0c636e29ecbb5344252a4a`,((urlErr,urlRes)=>{
         // request(`https://api.douban.com/v2/book/search?apikey=0df993c66c0c636e29ecbb5344252a4a&start=${Number.parseInt(Math.random()*1000%200)}&count=3&tag=${encodeURI('前端')}`,((urlErr,urlRes)=>{
         //     if(urlErr)throw urlErr
         //     else {
@@ -12,7 +12,7 @@ const dealBarcode = (barcode,reply,models,open_id)=>request(`https://api.douban.
             else {
                 // console.log(JSON.parse(urlRes.body))
                 let imgUrl = JSON.parse(urlRes.body).images.small
-                let obj = {...JSON.parse(urlRes.body),imgUrl,open_id}
+                let obj = {...JSON.parse(urlRes.body),imgUrl,open_id,collected:!!join}
                 // console.log(obj)
                 if(obj.title==='Undefined'){
                     reply({
@@ -23,7 +23,7 @@ const dealBarcode = (barcode,reply,models,open_id)=>request(`https://api.douban.
                 }else{
                     let model = new models.goods(obj);
                     console.log(model,'??')
-                    model.save()
+                    if(!!join)model.save()
                     reply({
                         status:200,
                         data:obj,
