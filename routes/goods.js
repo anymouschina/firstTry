37830e87792034bd276349362dcbe18b0720b6e5
unknown <624144061@qq.com>
2019-12-25 17:23:24 +0800

    添加批量删除

--- routes/goods.js
+++ routes/goods.js
@@ -42,7 +42,7 @@ module.exports = [
       if(tags){
         defaultTags = [...defaultTags,...tags];
       }
-      if(title==='')getgoodsInfo.dealBooks(defaultTags[Number.parseInt(Math.random()*100000%(defaultTags.length))],count,reply)
+      if(title==='string')getgoodsInfo.dealBooks(defaultTags[Number.parseInt(Math.random()*100000%(defaultTags.length))],count,reply)
       else{
         getgoodsInfo.searchBooks(title,count,reply)
       }
