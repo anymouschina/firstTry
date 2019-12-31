b4d95399404c1791e06a9cbbac5b224147187907
unknown <624144061@qq.com>
2019-12-31 19:29:50 +0800

    add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Hapi = require('hapi');
-const hapiAuthJWT2 = require('hapi-auth-jwt2');
+const Hapi = require('@hapi/hapi');
 const H2o2 = require('@hapi/h2o2');
+const hapiAuthJWT2 = require('hapi-auth-jwt2');
 require('env2')('./.env');
 const config = require('./config');
 const routesUsers = require('./routes/users');
@@ -9,47 +9,48 @@ const routesGoods = require('./routes/goods')
 const pluginHapiSwagger = require('./plugins/hapi-swagger');
 const pluginHapiPagination = require('./plugins/hapi-pagination');
 const pluginHapiAuthJWT2 = require('./plugins/hapi-auth-jwt2');
-const server = new Hapi.Server();
-// 配置服务器启动host与端口
-server.connection({
+const server = Hapi.server({
   port: 3000,
-  host: '127.0.0.1',
+  host: 'localhost'
 });
 const init = async () => {
   // 注册插件
-  await server.register([
-    ...pluginHapiSwagger,
-    pluginHapiPagination,
-    hapiAuthJWT2
-  ]);
-  await server.register(H2o2);
-  pluginHapiAuthJWT2(server);
-  // 注册路由
-  server.route([
-    // 创建一个简单的hello hapi接口
-    ...routesUsers,
-    ...routesVotings,
-    ...routesGoods,{
-      method: 'GET',
-      path: '/wxserver/{name}',
-      handler: {
-          proxy: {
-              uri: 'https://amlie.oicp.vip/{name}'
+  try {
+    await server.register([H2o2,
+      ...pluginHapiSwagger,
+      pluginHapiPagination,
+      hapiAuthJWT2]);
+      pluginHapiAuthJWT2(server);
+       // 注册路由
+      server.route([
+        // 创建一个简单的hello hapi接口
+        ...routesUsers,
+        ...routesVotings,
+        ...routesGoods,{
+          method: 'GET',
+          path: '/wxserver/{name}',
+          handler: {
+              proxy: {
+                  uri: 'https://amlie.oicp.vip/{name}'
+              }
           }
-      }
-  },{
-    method: 'POST',
-    path: '/wxserver/{name}',
-    handler: {
-        proxy: {
-            uri: 'https://amlie.oicp.vip/{name}'
+      },{
+        method: 'POST',
+        path: '/wxserver/{name}',
+        handler: {
+            proxy: {
+                uri: 'https://amlie.oicp.vip/{name}'
+            }
         }
-    }
-},
-  ]);
-  // 启动服务
-  await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
+    },
+      ]);
+    await server.start();
+
+    console.log(`Server started at:  ${server.info.uri}`);
+  }
+  catch(e) {
+    console.log('Failed to load h2o2');
+  }
 };
 
 init();
