0e2d49740e6eb29ecd7b388eaef2d596d8675a3d
unknown <624144061@qq.com>
2019-12-31 19:33:17 +0800

    add

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const Hapi = require('@hapi/hapi');
-const H2o2 = require('@hapi/h2o2');
+const Hapi = require('hapi');
 const hapiAuthJWT2 = require('hapi-auth-jwt2');
 require('env2')('./.env');
 const config = require('./config');
@@ -9,48 +8,63 @@ const routesGoods = require('./routes/goods')
 const pluginHapiSwagger = require('./plugins/hapi-swagger');
 const pluginHapiPagination = require('./plugins/hapi-pagination');
 const pluginHapiAuthJWT2 = require('./plugins/hapi-auth-jwt2');
-const server = Hapi.server({
+const server = new Hapi.Server();
+// 配置服务器启动host与端口
+server.connection({
   port: 3000,
-  host: 'localhost'
+  host: '127.0.0.1',
 });
 const init = async () => {
   // 注册插件
-  try {
-    await server.register([H2o2,
-      ...pluginHapiSwagger,
-      pluginHapiPagination,
-      hapiAuthJWT2]);
-      pluginHapiAuthJWT2(server);
-       // 注册路由
-      server.route([
-        // 创建一个简单的hello hapi接口
-        ...routesUsers,
-        ...routesVotings,
-        ...routesGoods,{
-          method: 'GET',
-          path: '/wxserver/{name}',
-          handler: {
-              proxy: {
-                  uri: 'https://amlie.oicp.vip/{name}'
-              }
-          }
-      },{
-        method: 'POST',
-        path: '/wxserver/{name}',
-        handler: {
-            proxy: {
-                uri: 'https://amlie.oicp.vip/{name}'
-            }
+  await server.register([
+    ...pluginHapiSwagger,
+    pluginHapiPagination,
+    hapiAuthJWT2,
+  ]);
+  pluginHapiAuthJWT2(server);
+  // 注册路由
+  server.route([
+    // 创建一个简单的hello hapi接口
+    ...routesUsers,
+    ...routesVotings,
+    ...routesGoods,
+    {
+      method: 'GET',
+      path: '/wxserver/{name}',
+      handler: {
+        proxy: {
+          mapUri: function(request,callback){
+              callback(null,'https://amlie.oicp.vip/'+request.params.name);
+          },
+          onResponse: function(err, res, request, reply, settings, ttl){
+             console.log(res); 
+             reply(res);
+          },
+          passThrough: true,
+          xforward: true
         }
-    },
-      ]);
-    await server.start();
-
-    console.log(`Server started at:  ${server.info.uri}`);
-  }
-  catch(e) {
-    console.log('Failed to load h2o2');
-  }
+      }
+  },{
+    method: 'POST',
+    path: '/wxserver/{name}',
+    handler: {
+      proxy: {
+        mapUri: function(request,callback){
+            callback(null,'https://amlie.oicp.vip/'+request.params.name);
+        },
+        onResponse: function(err, res, request, reply, settings, ttl){
+           console.log(res); 
+           reply(res);
+        },
+        passThrough: true,
+        xforward: true
+      }
+    }
+},
+  ]);
+  // 启动服务
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 init();
