d1f7a38e15194f7c69471a32b509c9fea0b72b5d
unknown <624144061@qq.com>
2020-01-19 09:38:17 +0800

    addzhanghu

--- test.js
+++ test.js
@@ -2,24 +2,48 @@ const superagent = require('superagent')
 const cheerio = require('cheerio')
 function getHotComment(model,page,callback){
   superagent
-  .get('https://www.musicbooks.cn/page/2')
+  .get('https://flash-api.jin10.com/get_flash_list?max_time=2020-01-12+21%3A36%3A15&channel=-8200')
   .end((err,res) => {
-    const $ = cheerio.load(res.text);
-    let list = []
-     $('article').map((i,el)=>{
-       const html = $(el).children()
-       let textObj = {
-         '0':'title',
-         '1':'content',
-         '2':'beizhu'
-       }
-       let result = {}
-       html.map(index=>{
-         result[textObj[index]] = $(html[index]).text().trim()
-       })
-       list.push(result)
-     })
-     callback(list)
+    console.log(res.text)
+    // const $ = cheerio.load(res.text);
+    // let list = []
+    //  $('article').map((i,el)=>{
+    //    const html = $(el).children()
+    //    let textObj = {
+    //      '0':'title',
+    //      '1':'content',
+    //      '2':'beizhu'
+    //    }
+    //    let result = {}
+    //    html.map(index=>{
+    //      result[textObj[index]] = $(html[index]).text().trim()
+    //    })
+    //    list.push(result)
+    //  })
+    //  callback(list)
   })
   
 }
+superagent
+  .get('https://flash-api.jin10.com/get_flash_list?max_time=2020-01-12+21%3A36%3A15&channel=-8200')
+  .set('access-control-allow-origin','https://www.jin10.com')
+  .withCredentials()
+  .end((err,res) => {
+    console.log(res)
+    // const $ = cheerio.load(res.text);
+    // let list = []
+    //  $('article').map((i,el)=>{
+    //    const html = $(el).children()
+    //    let textObj = {
+    //      '0':'title',
+    //      '1':'content',
+    //      '2':'beizhu'
+    //    }
+    //    let result = {}
+    //    html.map(index=>{
+    //      result[textObj[index]] = $(html[index]).text().trim()
+    //    })
+    //    list.push(result)
+    //  })
+    //  callback(list)
+  })
