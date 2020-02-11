b9b08237cc275fecbdb57eaad2fc161f1f137e1e
unknown <624144061@qq.com>
2020-02-11 18:13:04 +0800

    1

--- utils/dealUser.js
+++ utils/dealUser.js
@@ -1,24 +1,19 @@
 
 const models = require('../models')
-function updateUser(models,content){
+async function updateUser(models,content){
     if(content.type===1){
-      const userChangeRecord =  new models.userChangeRecord({type:content.type,
+      const userChangeRecord = await  new models.userChangeRecord({type:content.type,
             content,
             open_id:content.open_id,
         })
-        userChangeRecord.save()
-        updateUserInfo(content.open_id,{skinChipNum:(num)=>{return num + content.num}})
+        userChangeRecord.save(
+            models.users.findOneAndUpdate({open_id:content.open_id},
+                 { $inc:{skinChipNum:content.num}} )
+        )
+        
     }else{
 
     }
-}
-function updateUserInfo(open_id,content){//更新用户表数据
-    models.users.find({open_id:open_id},(err,doc)=>{
-        Object.keys(content).map(item=>{
-            doc[item] = content[item](doc[item])
-        })
-        doc.save()
-    })
 }
 module.exports = {
     updateUser
