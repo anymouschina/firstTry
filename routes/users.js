0d97515a71eab6450cc5a72e8f68550cf44b3a8f
unknown <624144061@qq.com>
2020-02-11 14:16:52 +0800

    1

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ async function userLogin(reply,from = 0,result = []){
     const luckDraws = await models.luckDraws.find({isFinish:false}).map(item=>{
      return item.map(async n=>{
        const userJoin = (await models.usersJoinRecord.find({luckDrawId:n._id,open_id:result.open_id}).countDocuments())>0
-       console.log(userJoin,'11')
+       console.log(userJoin,n,'11')
          return {
           ...n,
           userJoin
