6b75c89d95f1017df42aa7bd546873e9c18e33f8
unknown <624144061@qq.com>
2020-02-13 14:51:06 +0800

    1

--- routes/luckDraws.js
+++ routes/luckDraws.js
@@ -10,7 +10,7 @@ module.exports = [
     path: `/${GROUP_NAME}/finish`,
     handler: async (request, reply) => {
     const {id} = request.query;
-    const list = await models[GROUP_NAME].findById(id)
+    const list = await models[GROUP_NAME].findById(id).updateOne({isFinish:true})
     const total = await models.usersJoinRecord.find({luckDrawId:id}).countDocuments()
     let num = 0;
     if(total<list._doc.prize.num){
