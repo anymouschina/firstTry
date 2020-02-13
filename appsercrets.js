5fda95cacad6997cc1a2b2324fdf7437ec2dfbf0
unknown <624144061@qq.com>
2020-02-13 18:16:24 +0800

    1

--- appsercrets.js
+++ appsercrets.js
@@ -1,10 +1,14 @@
 const config = [
     {appid: 'wxcb642de46f9fb4bf',
     secret: '94be7aeda8c1e5409f15d6ed09daa001'},
+    { appid:'wx294e8ff02bd08b38',
+      secret:'ed1f20018e259bb7fedf38928e4dee19'
+    },
     {
       appid:'wx536dfc5d38954079',
       secret:'ca008a164dfa01913a6ec8edbcbe2cf7'
     }
+    
   
 ];
   module.exports = config;
