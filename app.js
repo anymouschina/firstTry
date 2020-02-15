bee51ec2ab3a076ce479683dc1c31d598431d9ff
unknown <624144061@qq.com>
2020-02-15 11:11:43 +0800

    1

--- app.js
+++ app.js
@@ -23,8 +23,15 @@ schedule.scheduleJob('30 7 * * *',async ()=>{
   request({
     url:'https://www.saberc8.cn/users/resetRegister',
     method:'GET'
-  },()=>{console.log('每天0点置空签到时间')})
-  // await models.luckDraws.find({openTime:new Date()})
+  },()=>{console.log('每天7.30点置空签到时间')})
+  const time = new Date()
+  const findTime = new Array(time.getFullYear(),(parseInt(time.getMonth()+1)<10?'0':'').concat(parseInt(time.getMonth()+1)),new String(time.getDate()).concat('T00:00:00.000Z')).join('-')
+  await models.luckDraws.findOne({openTime:findTime},function(err,doc){
+    request({
+      url:'https://www.saberc8.cn/luckDraws/finish?id='+doc._id,
+      method:'GET'
+    },()=>{console.log('结束抽奖')})
+  })
 })
 const init = async () => {
   // 注册插件
