1588e286698b92973ab220193c801ef6f090b8ef
unknown <624144061@qq.com>
2020-02-16 08:54:28 +0800

    1

--- task.js
+++ task.js
@@ -3,9 +3,12 @@ const models = require('./models')
 const schedule = require('node-schedule');
 const time = new Date()
       const findTime = new Array(time.getFullYear(),(parseInt(time.getMonth()+1)<10?'0':'').concat(parseInt(time.getMonth()+1)),new String(time.getDate()+1)).join('-')
-    models.luckDraws.findOne({$or: [  // 多字段同时匹配
+      console.log(findTime,'??')
+    models.luckDraws.findOne({
+    $or: [  // 多字段同时匹配
         {openTime: {$regex: findTime}}
       ]},function(err,doc){
+          if(err)throw err
         console.log(doc)
     })
 // schedule.scheduleJob('30 7 * * *',async ()=>{
