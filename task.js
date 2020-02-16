e0e959ee3e5811336ce7613a6f6d46e86daeb75d
unknown <624144061@qq.com>
2020-02-16 08:52:32 +0800

    1

--- task.js
+++ task.js
@@ -1,6 +1,7 @@
 
 const models = require('./models')
 const schedule = require('node-schedule');
+const time = new Date()
       const findTime = new Array(time.getFullYear(),(parseInt(time.getMonth()+1)<10?'0':'').concat(parseInt(time.getMonth()+1)),new String(time.getDate()+1)).join('-')
     models.luckDraws.findOne({$or: [  // 多字段同时匹配
         {openTime: {$regex: findTime}}
